fdc245a0b6f3da8966f5113c5812af341ff4d396
Eveke <kelseaeverett@gmail.com>
2016-02-22 18:28:27 -0800

    Updated with final project starter files

--- class_11_arrays/carousel/js/index.js
+++ class_11_arrays/carousel/js/index.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 // Instructions:
 // In the images folder, you will find 8 food images (feel free to use other images if you want)
 // By default, food1.jpg is shown on the page.
@@ -12,30 +14,67 @@
 
 
 
+// var counter = 0;
+//
+// var images = ['food1.jpg',
+//               'food2.jpg',
+//               'food3.jpg',
+//               'food4.jpg',
+//               'food5.jpg',
+//               'food6.jpg',
+//               'food7.jpg',
+//               'food8.jpg']
+//
+// $('.next').click(function () {
+//       $('.image').attr('src','images/' + images[counter = counter + 1])
+//
+// });
+//
+// $('.back').click(function () {
+//       $('.image').attr('src','images/' + images[counter = counter - 1])
+//
+// });
+//
+//
+// $('.next').click(function () {
+//       counter = counter + 1;
+//       $('.image').attr('src', images[counter])
+//
+// });
+
 var counter = 0;
 
-var images = ['food1.jpg',
-              'food2.jpg',
-              'food3.jpg',
-              'food4.jpg',
-              'food5.jpg',
-              'food6.jpg',
-              'food7.jpg',
-              'food8.jpg']
+var images = [
+  'images/food1.jpg',
+  'images/food2.jpg',
+  'images/food3.jpg',
+  'images/food4.jpg',
+  'images/food5.jpg',
+  'images/food6.jpg',
+  'images/food7.jpg',
+  'images/food8.jpg'
+];
 
-$('.next').click(function () {
-      $('.image').attr('src','images/' + images[counter = counter + 1])
-
-});
-
-$('.back').click(function () {
-      $('.image').attr('src','images/' + images[counter = counter - 1])
+$.each(images, function(index, value) {
+  $('.image-list').append('<li>' + images[index] + '</li>'); //string concatenation
+  // $('li').attr('src', images[index]);
 
 });
 
 
-$('.next').click(function () {
-      counter = counter + 1;
-      $('.image').attr('src', images[counter])
+// $('.next').click(function() {
+//   if (counter < images.length - 1) {
+//     counter = counter + 1;
+//   }
+//
+//   $('.image').attr('src', images[counter]);
+// });
+//
+// $('.back').click(function() {
+//   counter = counter - 1;
+//
+//   $('.image').attr('src', images[counter]);
+// });
 
-});
+
+}); //end ready
